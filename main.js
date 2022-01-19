063ca3a80664511412e22d3ac859369669ea185b
ShafiAnwar <92093225+ShafiAnwar@users.noreply.github.com>
2022-01-19 18:44:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+Webcam.set({
+    height:300,
+    width:350,
+    image_format:'png',
+    png_quality:100
+});
+
+Webcam.attach("#camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML="<img id='captured_image' src='"+data_uri+"'/>"; 
+    });
+}
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/KWaHtIvfc/model.json', modelLoaded);
+
+function modelLoaded(){
+    console.log('ModelLoaded!');
+}
